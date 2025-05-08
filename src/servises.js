6125927662401de105a729a4e2d0df1f135918d1
Sai-Prabhav <saiprabhav.ss@gmail.com>
2025-05-08 19:51:42 +0530

    Update API endpoints in servises.js to use a dynamic URI; improve code maintainability.

--- src/servises.js
+++ src/servises.js
@@ -1,7 +1,9 @@
 import { data } from "react-router-dom";
 
+const URI = "https://ancient-demetra-sai-prabhav-505b893a.koyeb.app/";
+
 const login = async (name, password) => {
-  const response = await fetch("http://127.0.0.1:5000/login", {
+  const response = await fetch(`https://${URI}/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -14,7 +16,7 @@ const login = async (name, password) => {
 };
 
 const register = async (name, password) => {
-  const response = await fetch("http://127.0.0.1:5000/register", {
+  const response = await fetch(`https://${URI}/register`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -29,7 +31,7 @@ const register = async (name, password) => {
 };
 
 const get_notes = async (token) => {
-  const response = await fetch("http://127.0.0.1:5000/get-notes", {
+  const response = await fetch(`https://${URI}/get-notes`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -42,7 +44,7 @@ const get_notes = async (token) => {
 };
 
 const save_notes = async (token, notes) => {
-  const response = await fetch("http://127.0.0.1:5000/save-notes", {
+  const response = await fetch(`https://${URI}/save-notes`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
